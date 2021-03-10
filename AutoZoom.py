e0b57be2c2ad5f6225accd548e9bc1415bb7ecb7
warren yun <xxwarrenhdxx@gmail.com>
2021-03-09 23:57:18 -0500

    first commit

---
+++ AutoZoom.py
@@ -0,0 +1,20 @@
+import webbrowser
+import schedule
+import time
+
+def open_class(url):
+    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+    webbrowser.get('chrome').open(url)
+
+def main():
+    schedule.every().day.at("09:20").do(open_class, '')
+    schedule.every().day.at("10:07").do(open_class, '')
+    schedule.every().day.at("11:40").do(open_class, '')
+    schedule.every().day.at("12:28").do(open_class, '')
+    schedule.every().day.at("13:14").do(open_class, '')
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+main()
