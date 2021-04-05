00796e63103d49c6543039ae5298fdbbc17fa16a
warren yun <xxwarrenhdxx@gmail.com>
2021-04-05 16:29:02 -0400

    basic structure of GUI

--- AutoZoom.py
+++ AutoZoom.py
@@ -1,20 +1,76 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot, QObject, QThread, pyqtSignal
+
 import webbrowser
 import schedule
 import time
+import sys
 
-def open_class(url):
-    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
-    webbrowser.get('chrome').open(url)
+# thread running all pending scheduled classes
+class Worker(QObject):
+    def run(self):
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
-def main():
-    schedule.every().day.at("09:20").do(open_class, '')
-    schedule.every().day.at("10:07").do(open_class, '')
-    schedule.every().day.at("11:40").do(open_class, '')
-    schedule.every().day.at("12:28").do(open_class, '')
-    schedule.every().day.at("13:14").do(open_class, '')
+    
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'AutoZoom'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+        self.schedule_thread()
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    # GUI
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        self.name = QLineEdit(self)
+        self.name.move(20, 20)
+        self.name.resize(280, 40)
 
-main()
+        self.time = QLineEdit(self)
+        self.time.move(20, 90)
+        self.time.resize(280, 40)
+
+        self.link = QLineEdit(self)
+        self.link.move(20, 160)
+        self.link.resize(280, 40)
+
+        # send button
+        button = QPushButton("Schedule", self)
+        button.setToolTip("Adds the class to your schedule")
+        button.move(20, 220)
+        button.clicked.connect(self.on_click)
+
+        self.show()
+
+    # connecting GUI to worker thread
+    def schedule_thread(self):
+        self.thread = QThread()
+        self.worker = Worker()
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.thread.start()
+
+    # opens class meeting link
+    def open_class(url):
+        webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+
+    @pyqtSlot()
+    def on_click(self):
+        self.name.setText("")
+        self.time.setText("")
+        self.link.setText("")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
